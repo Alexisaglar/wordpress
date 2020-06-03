df373950bd2404c4be5fa223eb55c42540a8d9c9
alexisaglar <alexisaglar@gmail.com>
2020-06-02 22:25:08 -0500

    first commit

---
+++ deployment.py
@@ -0,0 +1,78 @@
+
+import subprocess
+from subprocess import check_output
+import sys
+import os
+import requests
+import fileinput
+
+def install_docker(user):
+    subprocess.run(['sudo apt update'], shell=True, check=True)
+    #Instalar algunos pre-requisitos para usar los paquetes por HTTPS:
+    subprocess.run(['sudo apt install apt-transport-https ca-certificates curl software-properties-common'], shell=True, check=True)
+    #Agregar la GPG key para el repositorio oficial de Docker al sistema:
+    subprocess.run(['curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'], shell=True, check=True)
+    #Agregar el repositorio de Docker a APT sources:
+    subprocess.run(['sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"'], shell=True, check=True)
+    #Update
+    subprocess.run(['sudo apt update'], shell=True, check=True)
+    #Finalmente se instala Docker
+    subprocess.run(['sudo apt install docker-ce'], shell=True, check=True)
+    #Para evitar escribir sudo cada que se utilice docker agrega el username al docker group
+    subprocess.run(['sudo usermod -aG docker ' + user], shell=True, check=True)
+    print('Instalacion docker OKAY')
+    # subprocess.run(['su - ' + user], shell=True, check=True)
+    # print('8')
+    # subprocess.run(['id -nG'], shell=True, check=True)
+    # print('9')
+
+def install_compose():
+    #Checar la ultima versión y actualizar el comando
+    subprocess.run(['sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose'], shell=True, check=True)
+    #Checar la ultima versión y actualizar el comando
+    subprocess.run(['sudo chmod +x /usr/local/bin/docker-compose'], shell=True, check=True)
+    #Agregar permisos de docker
+    subprocess.run(['docker-compose --version'], shell=True, check=True)
+    print('Instalacion Docker-Compose OKAY')
+
+def get_ip():
+    subprocess.run(['sudo pip install requests'], shell=True, check=True)
+    r = requests.get(r'http://jsonip.com')
+    ip= r.json()['ip']
+    print(ip)
+    return(ip)
+    #result = subprocess.run(['curl https://ipinfo.io/ip'], shell=True, check=True, capture_output=True)
+    #process = subprocess.run(['curl https://ipinfo.io/ip'], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #output = subprocess.check_output('curl https://ipinfo.io/ip', shell=True)
+
+def replace_file():
+    ip = get_ip()
+    with fileinput.FileInput("nginx.conf", inplace=True, backup='.bak') as file:
+        for line in file:
+            print(line.replace("__ip__", ip), end='')
+    print('---- ip has been replaced correctly -----')
+
+def move_files():
+    path = os.getcwd()
+    subprocess.run(['docker cp %s/html wordpress:/var/www/html'%path], shell=True, check=True)
+    print('---- html files have been replaced -----')
+
+def docker_compose():
+    #Agregar permisos de docker
+    subprocess.run(['docker-compose up -d'], shell=True, check=True)
+    #Agregar permisos de docker
+    subprocess.run(['docker-compose ps'], shell=True, check=True)
+    print('---- docker containers have been set -----')
+
+
+def main(argv):
+    user = sys.argv[1]
+    #install_docker(user)
+    #install_compose()
+    docker_compose()
+    #replace_file()
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ html/wp-config.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'bitnami_wordpress' );
+
+/** MySQL database username */
+define( 'DB_USER', 'bn_wordpress' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', '35b8dde584' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost:3306' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY', 'eece02794073ae67543daf52001e8bffd2be60a3b6cbffc5e875745c927fefc0');
+define('SECURE_AUTH_KEY', 'ddce0bc9c74de11a194a088dcfc197ea99274bd727b9660174178b426fb4f04c');
+define('LOGGED_IN_KEY', 'e614c1dde6d662be3dad2635a46191e596884799244878f4a7cea0a999834fdb');
+define('NONCE_KEY', 'edd1835251d62a2b2757bdc97dd6daea78e48e5df27f15eaf91a196c361a3e24');
+define('AUTH_SALT', '9ef672563c11c7b985672ff6864ec455e7287ed8976b254adbf07e154f814758');
+define('SECURE_AUTH_SALT', '3b69e151ce393749ee51001ba933117e4acae9fe0332377435571dcaa60b037a');
+define('LOGGED_IN_SALT', '32fad3144c57191d99b135122bcbe78b2f1059de15565791aec6fe750d767d78');
+define('NONCE_SALT', 'cf14f0534c56c7bcc64b692db8a64cc72a9b75887df96ca6bb1de45cc80929dd');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+
+define('FS_METHOD', 'direct');
+
+/**
+ * The WP_SITEURL and WP_HOME options are configured to access from any hostname or IP address.
+ * If you want to access only from an specific domain, you can modify them. For example:
+ *  define('WP_HOME','http://example.com');
+ *  define('WP_SITEURL','http://example.com');
+ *
+*/
+
+if ( defined( 'WP_CLI' ) ) {
+    $_SERVER['HTTP_HOST'] = 'localhost';
+}
+
+define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
+
+define('WP_TEMP_DIR', '/opt/bitnami/apps/wordpress/tmp');
+
+
+//  Disable pingback.ping xmlrpc method to prevent Wordpress from participating in DDoS attacks
+//  More info at: https://docs.bitnami.com/general/apps/wordpress/troubleshooting/xmlrpc-and-pingback/
+
+if ( !defined( 'WP_CLI' ) ) {
+    // remove x-pingback HTTP header
+    add_filter('wp_headers', function($headers) {
+        unset($headers['X-Pingback']);
+        return $headers;
+    });
+    // disable pingbacks
+    add_filter( 'xmlrpc_methods', function( $methods ) {
+            unset( $methods['pingback.ping'] );
+            return $methods;
+    });
+    add_filter( 'auto_update_translation', '__return_false' );
+}
